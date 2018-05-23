d568940e68d2fac076a029a2c22b28c47349f1e4
Brandon Kerns <bkerns@uw.edu>
2018-05-23 11:43:17 -0700

    Path modifications. Don't use subdirectories.

--- src/preprocess_tmpa.m
+++ src/preprocess_tmpa.m
@@ -7,6 +7,7 @@ addpath('../config')
 options
 
 % Set variables that pertain to this step.
+% Note: ALL gridded rain rate files should be linked or copied into the "RAW_DATA_DIR"
 RAW_DATA_DIR = '../data/raw/tmpa';
 INTERIM_DATA_DIR = '../data/interim/gridded_rain_rates'
 
@@ -24,7 +25,8 @@ for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
     DD = sprintf('%02d',day);
     HH = sprintf('%02d', hour);
 
-    this_dir = [RAW_DATA_DIR,'/',YYYY,'/',MM,'/',YYYY,MM,DD];
+    %this_dir = [RAW_DATA_DIR,'/',YYYY,'/',MM,'/',YYYY,MM,DD];
+    this_dir = [RAW_DATA_DIR];
 
     this_raw_file = [this_dir,'/3B42.',YYYY,MM,DD,'.',HH,'.7.HDF'];
     this_interim_file = [INTERIM_DATA_DIR,...
@@ -43,20 +45,23 @@ for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
         this_rain(this_rain < -0.01) = NaN ;
 
         disp(this_raw_file) ;
+
+
+		       % Prepare struct and write interim output file.
+	fout.lon=F.lon ;
+	fout.lat=F.lat ;
+	fout.time=datenum(year,month,day,hour,0,0) ;
+	fout.rain=this_rain ;
+	fout.info.smoothing='none' ;
+	fout.info.accumulation='none' ;
+	fout.info.units='mm/day' ;
+	
+	eval(['!mkdir -p ',INTERIM_DATA_DIR])
+	eval(['save ',this_interim_file,' -struct fout'])
+
     else
-        disp(['No TMPA data found for ',YYYY,MM,DD,HH])
+        disp(['WARNING! No TMPA data found for ',YYYY,MM,DD,HH,'. Skipping.'])
     end
 
-    % Prepare struct and write interim output file.
-    fout.lon=F.lon ;
-    fout.lat=F.lat ;
-    fout.time=datenum(year,month,day,hour,0,0) ;
-    fout.rain=this_rain ;
-    fout.info.smoothing='none' ;
-    fout.info.accumulation='none' ;
-    fout.info.units='mm/day' ;
-
-    eval(['!mkdir -p ',INTERIM_DATA_DIR])
-    eval(['save ',this_interim_file,' -struct fout'])
 
 end
