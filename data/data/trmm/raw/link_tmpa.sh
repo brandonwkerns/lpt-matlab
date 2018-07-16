a5cfaa2d3f1fc401c764534b544d24abc959847e
Brandon Kerns <bkerns@uw.edu>
2018-07-16 11:42:48 -0700

    Script to link TRMM 3B42 files.

---
+++ data/data/trmm/raw/link_tmpa.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+## Link TMPA data on orca.
+## Edit data directory and years below.
+
+data_dir=/home/orca/data/satellite/trmm_global_rainfall
+year1=2017
+year2=2018
+
+
+############### End of editing. ##########################
+
+# Clean up
+rm -f *.HDF
+
+
+# Link Files
+for MM in 06 07 08 09 10 11 12
+do
+    ln -s $data_dir/$year1/$MM/*/*.HDF .
+done
+
+for MM in 01 02 03 04 05 06
+do
+    ln -s $data_dir/$year2/$MM/*/*.HDF .
+done
+
+
+
+exit 0
+
