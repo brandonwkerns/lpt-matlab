4883b389cf36cb92296708b31624f0137df176bb
Brandon Kerns <bkerns@uw.edu>
2018-07-16 12:03:13 -0700

    data/trmm not data/data/trmm.

--- data/trmm/raw/link_tmpa.sh
+++ data/trmm/raw/link_tmpa.sh
@@ -2,30 +2,19 @@
 
 ## Link TMPA data on orca.
 ## Edit data directory and years below.
-
+## NOTE: It is OK to link a longer time period than you will use for actual tracking.
 data_dir=/home/orca/data/satellite/trmm_global_rainfall
 year1=2017
 year2=2018
-
 
 ############### End of editing. ##########################
 
 # Clean up
 rm -f *.HDF
 
-
 # Link Files
-for MM in 06 07 08 09 10 11 12
-do
-    ln -sv $data_dir/$year1/$MM/*/*.HDF .
-done
-
-for MM in 01 02 03 04 05 06
-do
-    ln -sv $data_dir/$year2/$MM/*/*.HDF .
-done
-
-
+ln -sv $data_dir/$year1/*/*/*.HDF .
+ln -sv $data_dir/$year2/*/*/*.HDF .
 
 exit 0
 
