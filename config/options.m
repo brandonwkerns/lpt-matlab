cfafa08db64877f07f4538d2bf51a6f0905d0a07
Brandon Kerns <bkerns@uw.edu>
2018-07-16 11:33:40 -0700

    Update options file.

--- config/options.m
+++ config/options.m
@@ -1,11 +1,11 @@
 % This file contains the options and tracking criteria used for the LPT
 % tracking. Load this file at the top of each of the LPT scripts.
 
-
 CASE_LABEL = 'trmm'   % Data goes in data/case/[raw|interim|processed]
 
-
+%%
 %% Data Criteria
+%%
 
 % Rainfall data resolution (in degrees lat/lon and hours).
 % This is the resultion of the regular grid the data are interpolate to,
@@ -13,23 +13,30 @@ CASE_LABEL = 'trmm'   % Data goes in data/case/[raw|interim|processed]
 % NOTE: I have only done LPT with 0.25 deg. data. But for time intervals
 %       I have done 3 h for TRMM, 1 h for WRF ARW, and 6 h for ECMWF.
 %       Also, LON and LAT below aren't used until the spatial filter step.
-USE_NATIVE_GRID = false;
-DX  =  0.25; % Grid resolution
+USE_NATIVE_GRID = false;   % Next 2 lines ignored if false. (Use native grid)
+NATIVE_LON_VARNAME = 'XLONG';
+NATIVE_LAT_VARNAME = 'XLAT';
+
+DX  = 0.25; % Grid resolution. This and next two lines ignored if USE_NATIVE_GRID = true.
 LON =  39.875 : DX : 178.875;  % Set the longitude grid.
 LAT = -25.875 : DX : 24.875;   % Set the latitude grid.
-DN1 = datenum(2017,7,1,0,0,0);      % Set the starting time as a datenum.
-DN2 = datenum(2018,2,28,21,0,0);    % Set the ending time as a datenum.
-DT  = 3.0;                           % Set the time interval.
 
+% Time settings
+DN1 = datenum(2017,6,1,0,0,0);      % Set the starting time as a datenum.
+DN2 = datenum(2018,6,30,21,0,0);    % Set the ending time as a datenum.
+DT  = 3.0;                           % Set the time interval (hours).
+
+%%
 %% Accumulation and filtering criteria
+%%
 
 % Accumulation period, in hours.
 ACCUMULATION_PERIOD = 72.0;
 
 % Spatial filter (Gaussian bell shape) settings.
 % Standard deviation is in lat/lon.
-% Filter width is in terms of how many standard deviations.
-FILTER_STANDARD_DEVIATION = 5.0;
+% Filter (half) width is in terms of how many standard deviations.
+FILTER_STANDARD_DEVIATION = 20;   % In terms of grid points
 FILTER_WIDTH = 3.0;
 
 % For WRF regional domains, it may be good to use "ghost points" so that the LPT
@@ -40,7 +47,9 @@ FILTER_WIDTH = 3.0;
 FILTER_USE_GHOST_POINTS = false;
 FILTER_NUMBER_OF_GHOST_POINTS = 10;
 
+%%
 %% Feature identification
+%%
 
 % Threshold value to define a feature
 % If the feature is > (<) the threshold value, set FEATURE_IS_GT_VALUE to true (false).
@@ -57,8 +66,9 @@ FEATURE_MINIMUM_SIZE = 400;
 % (Kerns and Chen [2016] used 15.0.)
 FEATURE_MAX_LAT = 15.0;
 
-
+%%
 %% Tracking criteria
+%%
 
 % Overlap criteria. Minimum Minimum feature size is based on the number of points.
 % If either the fraction or points overlap is satisfied, the LPT features
