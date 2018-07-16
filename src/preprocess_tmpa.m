4e94cfe612adaf9eca728f3835075ebc27b1e5b8
Brandon Kerns <bkerns@uw.edu>
2018-07-16 12:02:17 -0700

    Using CASE_LABEL directory.

--- src/preprocess_tmpa.m
+++ src/preprocess_tmpa.m
@@ -8,15 +8,15 @@ options
 
 % Set variables that pertain to this step.
 % Note: ALL gridded rain rate files should be linked or copied into the "RAW_DATA_DIR"
-RAW_DATA_DIR = '../data/raw/tmpa';
-INTERIM_DATA_DIR = '../data/interim/gridded_rain_rates'
+RAW_DATA_DIR = ['../data/',CASE_LABEL,'/raw'];
+INTERIM_DATA_DIR = '../data/',CASE_LABEL,'/interim/gridded_rain_rates'
 
 %-----------------------------------------------------------------------------
 %-----------------------------------------------------------------------------
 %-----------------------------------------------------------------------------
 
-% NOTE: Start 3 days before DN1 to make sure a 3-day accumulation can be calculated.
-for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
+% NOTE: Start "ACCUMULATION_PERIOD_ hours before DN1 to make sure a 3-day accumulation can be calculated.
+for dn = DN1-ACCUMULATION_PERIOD/24.0:datenum(0,0,0,DT,0,0):DN2
 
     [year,month,day,hour] = datevec(dn);
 
@@ -25,7 +25,6 @@ for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
     DD = sprintf('%02d',day);
     HH = sprintf('%02d', hour);
 
-    %this_dir = [RAW_DATA_DIR,'/',YYYY,'/',MM,'/',YYYY,MM,DD];
     this_dir = [RAW_DATA_DIR];
 
     this_raw_file = [this_dir,'/3B42.',YYYY,MM,DD,'.',HH,'.7.HDF'];
@@ -47,7 +46,7 @@ for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
         disp(this_raw_file) ;
 
 
-		       % Prepare struct and write interim output file.
+	% Prepare struct and write interim output file.
 	fout.lon=F.lon ;
 	fout.lat=F.lat ;
 	fout.time=datenum(year,month,day,hour,0,0) ;
@@ -59,6 +58,12 @@ for dn = DN1-3.0:datenum(0,0,0,DT,0,0):DN2
 	eval(['!mkdir -p ',INTERIM_DATA_DIR])
 	eval(['save ',this_interim_file,' -struct fout'])
 
+
+
+	%% NetCDF output.
+	
+
+	
     else
         disp(['WARNING! No TMPA data found for ',YYYY,MM,DD,HH,'. Skipping.'])
     end
