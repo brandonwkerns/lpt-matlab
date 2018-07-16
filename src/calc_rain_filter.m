608688b953c0c3259ccbb30d38cf0a2fe6bb4822
Brandon Kerns <bkerns@uw.edu>
2018-07-16 12:07:25 -0700

    Clean up.

--- src/calc_rain_filter.m
+++ src/calc_rain_filter.m
@@ -36,7 +36,7 @@ for dn = DN1:datenum(0,0,0,DT,0,0):DN2
     f.lon = ncread(this_interim_file_in, 'lon');
     f.lat = ncread(this_interim_file_in, 'lat');
     f.rain = ncread(this_interim_file_in, 'rain')';
-    
+
     %This gets used for the subsetting by LON and LAT from options.m
     lonKeepIndx=find(f.lon > LON(1)-0.01 & f.lon < LON(end)+0.01) ;
     latKeepIndx=find(f.lat > LAT(1)-0.01 & f.lat < LAT(end)+0.01) ;
@@ -46,21 +46,6 @@ for dn = DN1:datenum(0,0,0,DT,0,0):DN2
     filter_std_points = FILTER_STANDARD_DEVIATION/DX;
 
     RAINFILTER=gaussSmooth(f.rain,filter_nx,filter_nx,filter_std_points,filter_std_points) ;
-
-    %{
-    fout.lon=f.lon(lonKeepIndx);
-    fout.lat=f.lat(latKeepIndx);
-    fout.time=dn;
-    fout.rain=RAINFILTER(latKeepIndx,lonKeepIndx);
-    fout.info.filter=['Gaussian, ',num2str(FILTER_STANDARD_DEVIATION),...
-        ' deg std dev., extending ',num2str(FILTER_WIDTH),'X std dev.'];
-    fout.info.accumulation='3 days prior' ;
-    fout.info.units='mm/day' ;
-
-    eval(['!mkdir -p ',INTERIM_DATA_DIR_OUT]) ;
-    eval(['save ',this_interim_file_out,' -struct fout'])
-
-    %}
 
     %% NetCDF output.
 
@@ -80,7 +65,7 @@ for dn = DN1:datenum(0,0,0,DT,0,0):DN2
     varid_rain = netcdf.defVar(ncid, 'rain', 'NC_DOUBLE', [dimid_lon dimid_lat dimid_time]);
     %netcdf.putAtt(ncid,varid_time,'units','seconds since 1970-1-1 0:0:0');
     %netcdf.putAtt(ncid,varid_rain,'units','mm day-1');
-    
+
     netcdf.endDef(ncid)
 
     % Data Mode
@@ -88,17 +73,17 @@ for dn = DN1:datenum(0,0,0,DT,0,0):DN2
     netcdf.putVar(ncid, varid_lat, f.lat(latKeepIndx));
     netcdf.putVar(ncid, varid_time, 0,1, 86400.0 * (dn - datenum(1970,1,1,0,0,0)));
     netcdf.putVar(ncid, varid_rain, RAINFILTER(latKeepIndx,lonKeepIndx)');
-    
+
     netcdf.close(ncid)
-    
+
     ncwriteatt(this_interim_file_out,'time','units','seconds since 1970-1-1 0:0:0');
     ncwriteatt(this_interim_file_out,'rain','units','mm day-1');
     ncwriteatt(this_interim_file_out,'/','creation_date',datestr(now));
     ncwriteatt(this_interim_file_out,'/','smoothing',['Gaussian, ',num2str(FILTER_STANDARD_DEVIATION),...
         ' deg std dev., extending ',num2str(FILTER_WIDTH),'X std dev.']);
     ncwriteatt(this_interim_file_out,'/','accumulation', [num2str(ACCUMULATION_PERIOD), ' hours prior']);
-      
 
 
-    
+
+
 end
