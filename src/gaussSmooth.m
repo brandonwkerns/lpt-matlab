d5abbf4e711e0788b97dac029ba7a9839af32ccc
Brandon Kerns <bkerns@uw.edu>
2018-07-17 14:37:24 -0700

    Add ghost points option.

--- src/gaussSmooth.m
+++ src/gaussSmooth.m
@@ -1,8 +1,8 @@
-function dataSmooth = gaussSmooth(data,nx,ny,stdx,stdy) 
+function dataSmooth = gaussSmooth(data, stdx, stdy, nx, ny, ghost_points)
 
-% dataSmooth = gaussSmooth(data,nx,ny,stdx,stdy) 
+% dataSmooth = gaussSmooth(data,nx,ny,stdx,stdy)
 %    -- OR --
-% dataSmooth = gaussSmooth(data,stdev) 
+% dataSmooth = gaussSmooth(data,stdev)
 %
 % Data is a 2-D array
 % The Gaussian kernel is defined by: nx,ny,stdx,stdy
@@ -19,29 +19,34 @@ function dataSmooth = gaussSmooth(data,nx,ny,stdx,stdy)
 %        Matlab's built-in 'conv2' is called with the SHAPE option set to 'same'
 %
 % UPDATE 2012.11.12 you can call this in the simplified way:
-%   dataSmooth = gaussSmooth(data,stdev) 
+%   dataSmooth = gaussSmooth(data,stdev)
 %  and it will use stdev in both x and y direction, extending the
 %  kernel out to 3 stdev's.
 
+%% Manage optional args with default values.
 
-if ( nargin < 3 ) 
-    
-    kernel = gaussSmoothKernel(nx) ;
-    
+if ( nargin < 5 )
+  nx = round(6*stdx + 1);
+  ny = round(6*stdy + 1);
 else
 
-    if ( mod(nx,2) == 0 ) , nx=nx+1 ; end
-    if ( mod(ny,2) == 0 ) , ny=ny+1 ; end
+nx = round(nx);
+ny = round(ny);
+stdx = round(stdx);
+stdy = round(stdy);
 
+if ( mod(nx,2) == 0 ) , nx=nx+1 ; end
+if ( mod(ny,2) == 0 ) , ny=ny+1 ; end
 
-    kernel = gaussSmoothKernel(nx,ny,stdx,stdy) ;
+kernel = gaussSmoothKernel(nx,ny,stdx,stdy) ;
 
+if ( nargin < 6 )
+  ghost_points = false;
 end
 
-
-dataSmooth=conv2(data, kernel, 'same') ;
-
-
-
-
-
+if ghost_points
+  data_with_ghost_points = [fliplr(data(:,1:nx)), data, data(:,end-nx,end)];
+  dataSmooth=conv2(data_with_ghost_points, kernel, 'same') ;
+else
+  dataSmooth=conv2(data, kernel, 'same') ;
+end
