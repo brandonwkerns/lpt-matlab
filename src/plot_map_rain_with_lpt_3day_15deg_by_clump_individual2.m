6d5e0e62b49f6b2373e21b306e013b9bb8430aa2
Brandon Kerns <bkerns@uw.edu>
2018-12-26 16:16:36 -0800

    Use mask method for plotting LPT rainfall.

--- src/plot_map_rain_with_lpt_3day_15deg_by_clump_individual2.m
+++ src/plot_map_rain_with_lpt_3day_15deg_by_clump_individual2.m
@@ -9,11 +9,15 @@ PROCESSED_DATA_DIR = ['../data/',CASE_LABEL,'/processed/',...
                        sprintf('%d',ACCUMULATION_PERIOD), ...
                        'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/timeclusters'];
 
+MASK_DIR = ['../data/',CASE_LABEL,'/processed/',...
+                      'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
+                       sprintf('%d',ACCUMULATION_PERIOD), ...
+                       'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/masks'];
+
 PLOT_DIR = ['../plots/',CASE_LABEL,'/processed/',...
                       'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
                        sprintf('%d',ACCUMULATION_PERIOD), ...
                        'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/systems/individual_clumps'];
-
 
 
 
@@ -29,8 +33,8 @@ colors=hsv(12);
 
 MJO=dlmread(['../data/',CASE_LABEL,'/processed/g20_72h/thresh12/identify_eastward_propagation/mjo_lpt_list.rejoin2.txt'],'',1,0);
 
-%for year1=[2001]
-for year1=[2007:2017]
+for year1=[2018]
+%for year1=[2007:2017]
 
   clf
 
@@ -42,8 +46,8 @@ for year1=[2007:2017]
   yyyy2=num2str(year2) ;
 
   y1_y2=[yyyy1,'_',yyyy2] ;
-  if year1 == 2017
-    y11_y22=[yyyy1,'060100_',yyyy2,'053121'] ;
+  if year1 == 2018
+    y11_y22=[yyyy1,'060100_',yyyy1,'112721'] ;
   else
     y11_y22=[yyyy1,'060100_',yyyy2,'063021'] ;
   end
@@ -51,14 +55,13 @@ for year1=[2007:2017]
 
   G=load([PROCESSED_DATA_DIR,'/TIMECLUSTERS_lpt_',y11_y22,'.rejoin2.mat']) ;
 
-  if isfield(G, 'TIMECLUSTERS2')
-    G.TIMECLUSTERS = [G.TIMECLUSTERS, G.TIMECLUSTERS2];
-  end
-  if isfield(G, 'TIMECLUSTERS3')
-    G.TIMECLUSTERS = [G.TIMECLUSTERS, G.TIMECLUSTERS3];
-  end
-  if isfield(G, 'TIMECLUSTERS4')
-    G.TIMECLUSTERS = [G.TIMECLUSTERS, G.TIMECLUSTERS4];
+
+  for iiii = 2:20
+
+    if isfield(G, ['TIMECLUSTERS', num2str(iiii)])
+      eval(['G.TIMECLUSTERS = [G.TIMECLUSTERS, G.TIMECLUSTERS', num2str(iiii),'];'])
+    end
+
   end
 
 
@@ -86,6 +89,8 @@ for year1=[2007:2017]
     dn2 = datenum(1900,1,1,0,0,0);
 
 
+    %% Plot area should be relevant for all the tracks in the "LPT system family."
+
     for ii=[lptid_for_this_clump]%1:numel(G.TIMECLUSTERS)
 
 
@@ -100,21 +105,52 @@ for year1=[2007:2017]
       lon2 = max([lon2, GG.lon]);
       lat1 = min([lat1, GG.lat]);
       lat2 = max([lat2, GG.lat]);
-      dn1 = min([dn1, GG.date]);
-      dn2 = max([dn2, GG.date]);
+      dn1 = min([dn1, GG.time]) - 3.0;  % Include accumulation period preceeding LPT system.
+      dn2 = max([dn2, GG.time]);
       
     end
 
-    plot_map_background([lon1-5.0, lon2+5.0, lat1-5.0, lat2+5.0]);
-    hold on
-
-
 
     %% Rain
+    
+    %% Using mask.
+    %% But start with "grand mask"
+    grand_master_times = dn1:0.125:dn2;
+    grand_master_mask = zeros(numel(grand_master_times), 400, 1440);
+    
+    for ii=[lptid_for_this_clump]    
+      this_mask.file = [MASK_DIR,'/lpt_system_mask_',yyyy1,'_',sprintf('%03d',ii),'.nc'] ;
+      this_mask.lon = ncread(this_mask.file, 'lon');
+      this_mask.lat = ncread(this_mask.file, 'lat');
 
-    count = 0;
+      this_mask.time = ncread(this_mask.file, 'time');
+      this_mask.dn = datenum(1970,1,1,0,0,0) + this_mask.time / 24.0 ;
+      this_mask.mask = ncread(this_mask.file, 'mask_with_filter_and_accumulation');
+      this_mask.mask = permute(this_mask.mask, [3,2,1]);
 
-    for dn_rain = dn1:0.125:dn2
+      this_beginning_time_indx = find(grand_master_times == this_mask.dn(1));
+      this_end_time_indx = find(grand_master_times == this_mask.dn(end));
+
+      this_time_indx_range = this_beginning_time_indx:this_end_time_indx;
+
+      grand_master_mask(this_time_indx_range,:,:) = this_mask.mask;
+      
+    end
+
+    % Get coordinates of the lon/lat extent of the "grand master mask."
+    max_mask = squeeze(max(grand_master_mask));
+    max_mask_x = squeeze(max(max_mask));
+    max_mask_y = squeeze(max(max_mask'));
+    lon1 = min(this_mask.lon(max_mask_x > 0));
+    lon2 = max(this_mask.lon(max_mask_x > 0));
+    lat1 = min(this_mask.lat(max_mask_y > 0));
+    lat2 = max(this_mask.lat(max_mask_y > 0));
+
+    %% Use the mask to get the rain from this LPT system family.
+    %count = 0; % Will divide to get mean.
+    idx = 1;
+    
+    for dn_rain = grand_master_times
 
       [y_rain, m_rain, d_rain, h_rain] = datevec(dn_rain);
       fileRain = ['../data/trmm/interim/gridded_rain_rates/gridded_rain_rates_',...
@@ -127,48 +163,47 @@ for year1=[2007:2017]
       F.lon=ncread(fileRain,'lon');
       F.lat=ncread(fileRain,'lat');
 
-
-      if count < 1
+      this_time_eliminate_mask = (squeeze(grand_master_mask(idx,:,:)) < 1);
+      F.precip(this_time_eliminate_mask) = 0.0;
+      
+      if idx == 1
 	rain_sum = 3.0 * F.precip;
       else
 	rain_sum = rain_sum + 3.0 * F.precip;
       end
       
-      count = count + 1;
-
+      idx = idx + 1;
+      
     end
     
-
-    rain_sum = rain_sum;
-				%F.rain(F.rain < 0.25) = NaN ;
     rain_sum(rain_sum < 10) = NaN ;
-    
     hold on
     
     DATA=rain_sum ; %log(rain_sum) ;
 
+    
 
+    %%
+    %% Begin plot
+    %%
+    plot_map_background([lon1-5.0, lon2+5.0, lat1-5.0, lat2+5.0]);
+    hold on
+
+    
     pcolor(F.lon, F.lat, DATA) ;
     hold on
 
     shading flat
-				%caxis([log(0.2),log(5)])
     caxis([0,1000.0])
     
     cmap=dlmread('cmap_rain.dat') ;
 				% colormap(cmap(9:end,:))
-    colormap(flipud(gray()))
+    %colormap(flipud(gray()))
+    colormap(cmap)
 
 
-
-
-
-
-
-
-
-
-
+    %% Outline of spatial influence of the LPT family.
+    contour(this_mask.lon, this_mask.lat, max_mask, [0.5,0.5],'r-', 'linewidth',2);
     
     
     for ii=[lptid_for_this_clump]%1:numel(G.TIMECLUSTERS)
@@ -187,8 +222,8 @@ for year1=[2007:2017]
       thisCol = colors(1+mod(this_clump_id, 10),:);
       
 
-      plot(GG.lon, GG.lat, 'color',thisCol, 'linewidth', 6);
-      plot(GG.lon(1), GG.lat(1), 'ro','markerfacecolor','r', 'markersize', 15);
+      plot(GG.lon, GG.lat, 'color',thisCol, 'linewidth', 3);
+      plot(GG.lon(1), GG.lat(1), 'ko','markerfacecolor','c', 'markersize', 15, 'linewidth',0.5);
       plot(GG.lon(end), GG.lat(end), 'mx','markerfacecolor','m', 'markersize', 15, 'linewidth',3);
       
       hold on
@@ -233,8 +268,6 @@ for year1=[2007:2017]
     end
     
 
-    axis([lon1 - 5.0,lon2 + 5.0,lat1 - 5.0, lat2 + 5.0]);
-    daspect([1,1,1]);
 
 
 
@@ -245,7 +278,8 @@ for year1=[2007:2017]
     
         
     plot_map_background([lon1-5.0, lon2+5.0, lat1-5.0, lat2+5.0]);
-	
+    daspect([1,1,1]);
+    
     set(gca,'layer','top')
     set(gca,'FontSize',12)
     box on
