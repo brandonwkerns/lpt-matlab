218cae548c9c106884518847ef5240ca38b7a2ea
Brandon Kerns <bkerns@uw.edu>
2018-12-26 17:35:15 -0800

    Move to track directory.

--- src/track/run_tracking.m
+++ src/track/run_tracking.m
@@ -3,7 +3,7 @@ close all
 
 % Read in options that pertain to the entire tracking package.
 % These settings are all in ../config/options.m
-addpath('../config')
+addpath('../../config')
 options
 save('temp.mat');
 OPT = load('temp.mat');
@@ -11,12 +11,12 @@ eval('!rm temp.mat')
 
 % This script reads in the interim files from ../data/interim/gridded_rain_rates
 % and creates accumulated rain files in ../data/interim/accumulate_rain
-PROCESSED_DATA_DIR_IN = ['../data/',CASE_LABEL,'/processed/',...
+PROCESSED_DATA_DIR_IN = ['../../data/',CASE_LABEL,'/processed/',...
                         'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
                         sprintf('%d',ACCUMULATION_PERIOD), ...
                         'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/objects'];
 
-PROCESSED_DATA_DIR_OUT = ['../data/',CASE_LABEL,'/processed/',...
+PROCESSED_DATA_DIR_OUT = ['../../data/',CASE_LABEL,'/processed/',...
                         'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
                         sprintf('%d',ACCUMULATION_PERIOD), ...
                         'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/timeclusters'];
@@ -25,8 +25,8 @@ PROCESSED_DATA_DIR_OUT = ['../data/',CASE_LABEL,'/processed/',...
 %-----------------------------------------------------------------------------
 %-----------------------------------------------------------------------------
 
-dn0 = DN1;%f0.time;
-dn9 = DN2;%f9.time;
+dn0 = DN1;
+dn9 = DN2;
 
 [year0,month0,day0,hour0] = datevec(dn0);
 YYYY0 = sprintf('%d', year0);
--- src/track/run_tracking_trmm_all.m
+++ src/track/run_tracking_trmm_all.m
@@ -3,9 +3,19 @@ close all
 
 % Read in options that pertain to the entire tracking package.
 % These settings are all in ../config/options.m
-addpath('../config')
+addpath('../../config')
+
+options
+save('temp.mat');
+OPT = load('temp.mat');
+eval('!rm temp.mat')
+
+[year1,month1,day1,hour1] = datevec(DN1);
+[year2,month2,day2,hour2] = datevec(DN2);
 
 
+%% Over-ride the year, and run through the loop with the new years.
+%% NOTE: This assumes the day, month, hour are the same as in the config file.
 for YEAR1 = [1998:2000] %2000:2016
 
   disp('-------------------------------------------------------------------')
@@ -16,19 +26,17 @@ for YEAR1 = [1998:2000] %2000:2016
 
   YEAR2 = YEAR1 + 1;
 
-  options
-  save('temp.mat');
-  OPT = load('temp.mat');
-  eval('!rm temp.mat')
-
+  DN1 = datenum(YEAR1, month1, day1, hour1, 0, 0);
+  DN2 = datenum(YEAR2, month2, day2, hour2, 0, 0);
+  
   % This script reads in the interim files from ../data/interim/gridded_rain_rates
   % and creates accumulated rain files in ../data/interim/accumulate_rain
-  PROCESSED_DATA_DIR_IN = ['../data/',CASE_LABEL,'/processed/',...
+  PROCESSED_DATA_DIR_IN = ['../../data/',CASE_LABEL,'/processed/',...
                           'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
                           sprintf('%d',ACCUMULATION_PERIOD), ...
                           'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/objects'];
 
-  PROCESSED_DATA_DIR_OUT = ['../data/',CASE_LABEL,'/processed/',...
+  PROCESSED_DATA_DIR_OUT = ['../../data/',CASE_LABEL,'/processed/',...
                           'g',sprintf('%d',FILTER_STANDARD_DEVIATION), '_',...
                           sprintf('%d',ACCUMULATION_PERIOD), ...
                           'h/thresh',num2str(FEATURE_THRESHOLD_VALUE),'/timeclusters'];
@@ -37,8 +45,8 @@ for YEAR1 = [1998:2000] %2000:2016
   %-----------------------------------------------------------------------------
   %-----------------------------------------------------------------------------
 
-  dn0 = DN1;%f0.time;
-  dn9 = DN2;%f9.time;
+  dn0 = DN1;
+  dn9 = DN2;
 
   [year0,month0,day0,hour0] = datevec(dn0);
   YYYY0 = sprintf('%d', year0);
